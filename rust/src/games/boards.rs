85fa87e45d32672b445d70291d9921f35c281fa7
Daniel Mencl <menclda6@gmail.com>
2026-09-22 15:33:12 +0200

    WIP: Wispwood scoring

--- rust/src/games/boards.rs
+++ rust/src/games/boards.rs
@@ -2,8 +2,18 @@ use std::collections::{HashSet, VecDeque};
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy)]
 pub struct Coordinates {
-    row: usize,
-    col: usize,
+    pub row: usize,
+    pub col: usize,
+}
+
+impl Coordinates {
+    pub fn diagonal_index(&self) -> isize {
+        (self.row as isize) - (self.col as isize) 
+    }
+
+    pub fn antidiagonal_index(&self) -> usize {
+        self.row + self.col
+    }
 }
 
 pub fn same_row(first: &Coordinates, second: &Coordinates) -> bool {
@@ -12,6 +22,26 @@ pub fn same_row(first: &Coordinates, second: &Coordinates) -> bool {
 
 pub fn same_col(first: &Coordinates, second: &Coordinates) -> bool {
     first.col == second.col
+}
+
+pub fn diagonally_adjacent(first: &Coordinates, second: &Coordinates) -> bool {
+    first.row.abs_diff(second.row) <= 1 && first.col.abs_diff(second.col) <= 1
+}
+
+pub fn orthogonal_distance(first: &Coordinates, second: &Coordinates) -> usize {
+    first.row.abs_diff(second.row) + first.col.abs_diff(second.col)
+}
+
+pub fn orthogonally_adjacent(first: &Coordinates, second: &Coordinates) -> bool {
+    orthogonal_distance(first, second) <= 1
+}
+
+pub fn same_diagonal(first: &Coordinates, second: &Coordinates) -> bool {
+    first.diagonal_index() == second.diagonal_index()
+}
+
+pub fn same_antidiagonal(first: &Coordinates, second: &Coordinates) -> bool {
+    first.antidiagonal_index() == second.antidiagonal_index()
 }
 
 pub struct Board2D<T> {
@@ -27,11 +57,25 @@ impl<T: Clone> Board2D<T> {
 }
 
 impl<T> Board2D<T> {
-    fn coords_to_index(&self, coords: Coordinates) -> Option<usize> {
-        if coords.row >= self.size || coords.col >= self.size {
+    const ORTHOGONAL_OFFSETS: [(isize, isize); 4] = [
+        ( 0,  1),
+        ( 0, -1),
+        ( 1,  0),
+        (-1,  0),
+    ];
+
+    const DIAGONAL_OFFSETS: [(isize, isize); 4] = [
+        ( 1,  1),
+        ( 1, -1),
+        (-1,  1),
+        (-1,  -1),
+    ];
+
+    fn coords_to_index(&self, coord: Coordinates) -> Option<usize> {
+        if coord.row >= self.size || coord.col >= self.size {
             None
         } else {
-            Some(coords.row * self.size + coords.col)
+            Some(coord.row * self.size + coord.col)
         }
     }
 
@@ -113,7 +157,93 @@ impl<T> Board2D<T> {
         components
     }
 
-    pub fn get(&self, coords: Coordinates) -> Option<&T> {
-        self.coords_to_index(coords).map(|index| &self.tiles[index])
+    pub fn get(&self, coord: Coordinates) -> Option<&T> {
+        self.coords_to_index(coord).map(|index| &self.tiles[index])
+    }
+
+    fn offset(&self, coord: &Coordinates, offsets: (isize, isize)) -> Option<Coordinates> {
+        let (row_offset, col_offset) = offsets;
+        let new_row = coord.row as isize + row_offset;
+        if new_row < 0 || new_row > self.size as isize {
+            return None;
+        }
+
+        let new_col = coord.col as isize + col_offset;
+        if new_col < 0 || new_col > self.size as isize {
+            return None;
+        }
+
+        Some(Coordinates { row: new_row as usize, col: new_col as usize })
+    }
+
+    fn offsets_from(&self, coord: &Coordinates, all_offsets: impl IntoIterator<Item = (isize, isize)>) -> Vec<Coordinates> {
+        all_offsets
+            .into_iter()
+            .filter_map(|offset| self.offset(coord, offset))
+            .collect()
+    }
+
+    pub fn orthogonal_neighbors(&self, coord: &Coordinates) -> Vec<Coordinates> {
+        self.offsets_from(coord, Self::ORTHOGONAL_OFFSETS)
+    }
+
+    pub fn diagonal_neighbors(&self, coord: &Coordinates) -> Vec<Coordinates> {
+        self.offsets_from(coord, Self::DIAGONAL_OFFSETS)
+    }
+
+    pub fn count_tiles(&self, coords: impl IntoIterator<Item = Coordinates>, predicate: impl Fn(&T) -> bool) -> usize {
+        coords
+            .into_iter()
+            .filter_map(|coord| self.get(coord))
+            .filter(|tile| predicate(tile))
+            .count()
+    }
+
+    pub fn count_all_tiles(&self, predicate: impl Fn(&T) -> bool) -> usize {
+        let coords = self.all_coordinates();
+        coords
+            .into_iter()
+            .filter_map(|coord| self.get(coord))
+            .filter(|tile| predicate(tile))
+            .count()
+    }
+
+    pub fn row(&self, row: usize) -> Vec<Coordinates> {
+        (0..self.size).into_iter().map(|col| Coordinates{ row, col }).collect()
+    }
+
+    pub fn col(&self, col: usize) -> Vec<Coordinates> {
+        (0..self.size).into_iter().map(|row| Coordinates{ row, col }).collect()
+    }
+
+    pub fn diagonal(&self, diagonal: isize) -> Vec<Coordinates> {
+        self.all_coordinates()
+            .into_iter()
+            .filter(|coord| coord.diagonal_index() == diagonal)
+            .collect()
+    }
+
+    pub fn antidiagonal(&self, antidiagonal: usize) -> Vec<Coordinates> {
+        self.all_coordinates()
+            .into_iter()
+            .filter(|coord| coord.antidiagonal_index() == antidiagonal)
+            .collect()
+    }
+
+    pub fn line(&self, coord: Coordinates, offsets: (isize, isize)) -> Vec<Coordinates> {
+        let mut result = Vec::new();
+
+        while let Some(new_coord) = self.offset(&coord, offsets)  {
+            result.push(new_coord);
+        }
+
+        result
+    }
+
+    pub fn orthogonal_lines(&self, coord: Coordinates) -> Vec<Vec<Coordinates>> {
+        Self::ORTHOGONAL_OFFSETS
+            .into_iter()
+            .map(|offset| self.line(coord, offset))
+            .collect()
     }
 }
--- rust/src/games/wispwood/model.rs
+++ rust/src/games/wispwood/model.rs
@@ -1,9 +1,11 @@
+use std::collections::HashSet;
+
 use crate::games::boards::Coordinates;
 
 use super::super::boards::Board2D;
 use super::score::WispwoodScoringCards;
 
-#[derive(Clone)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub enum WispwoodTile {
     Empty,
     Tree,
@@ -18,21 +20,105 @@ impl WispwoodTile {
     pub fn is_jack(&self) -> bool {
         matches!(self, WispwoodTile::Jack)
     }
+
+    pub fn is_witch(&self) -> bool {
+        matches!(self, WispwoodTile::Witch)
+    }
+
+    pub fn is_orb(&self) -> bool {
+        matches!(self, WispwoodTile::Orb)
+    }
+
+    pub fn is_heart(&self) -> bool {
+        matches!(self, WispwoodTile::Heart)
+    }
+
+    pub fn is_wisp(&self) -> bool {
+        matches!(self, WispwoodTile::Jack | WispwoodTile::Witch | WispwoodTile::Orb | WispwoodTile::Heart)
+    }
+
+    pub fn is_tree(&self) -> bool {
+        matches!(self, WispwoodTile::Tree)
+    }
 }
 
 pub struct WispwoodBoard {
-    size: usize,
     tiles: Board2D<WispwoodTile>,
 }
 
 impl WispwoodBoard {
     fn new(size: usize) -> WispwoodBoard {
         let tiles = Board2D::new(size, WispwoodTile::Tree);
-        WispwoodBoard { size, tiles }
+        WispwoodBoard { tiles }
     }
 
     pub fn tile_locations(&self, predicate: impl Fn(&WispwoodTile) -> bool) -> Vec<Coordinates> {
         self.tiles.tile_coordinates(predicate)
+    }
+
+    pub fn neighbors(&self, coord: &Coordinates) -> Vec<Coordinates> {
+        self.tiles.orthogonal_neighbors(coord)
+    }
+
+    pub fn groups(&self, coords: Vec<Coordinates>) -> Vec<HashSet<Coordinates>> {
+        self.tiles.components(coords, |coord| self.neighbors(coord))
+    }
+
+    pub fn count_tiles(&self, coords: impl IntoIterator<Item = Coordinates>, predicate: impl Fn(&WispwoodTile) -> bool) -> usize {
+        self.tiles.count_tiles(coords, predicate)
+    }
+
+    pub fn count_all_tiles(&self, predicate: impl Fn(&WispwoodTile) -> bool) -> usize {
+        self.tiles.count_all_tiles(predicate)
+    }
+
+    pub fn wisp_types(&self, coords: impl IntoIterator<Item = Coordinates>) -> HashSet<WispwoodTile> {
+        coords
+            .into_iter()
+            .filter_map(|coord| self.tiles.get(coord))
+            .filter(|tile| tile.is_wisp())
+            .copied()
+            .collect()
+    }
+
+    pub fn surrounding(&self, coord: &Coordinates) -> Vec<Coordinates> {
+        let mut result = self.tiles.orthogonal_neighbors(coord);
+        result.extend(self.tiles.diagonal_neighbors(coord));
+        result
+    }
+
+    pub fn row(&self, row: usize) -> Vec<Coordinates> {
+        self.tiles.row(row)
+    }
+
+    pub fn col(&self, col: usize) -> Vec<Coordinates> {
+        self.tiles.col(col)
+    }
+
+    pub fn row_and_column(&self, coord: Coordinates) -> Vec<Coordinates> {
+        let mut result = self.row(coord.row);
+        result.extend(self.col(coord.col));
+        result
+    }
+
+    pub fn diagonal(&self, diagonal: isize) -> Vec<Coordinates> {
+        self.tiles.diagonal(diagonal)
+    }
+
+    pub fn antidiagonal(&self, antidiagonal: usize) -> Vec<Coordinates> {
+        self.tiles.antidiagonal(antidiagonal)
+    }
+
+    pub fn orthogonal_lines(&self, coord: Coordinates) -> Vec<Vec<Coordinates>> {
+        self.tiles.orthogonal_lines(coord)
+    }
+
+    pub fn get(&self, coord: Coordinates) -> Option<&WispwoodTile> {
+        self.tiles.get(coord)
+    }
+
+    pub fn below(&self, coord: Coordinates) -> Vec<Coordinates> {
+        self.tiles.line(coord, (-1, 0))
     }
 }
 
